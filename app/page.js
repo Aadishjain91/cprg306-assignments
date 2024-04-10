20efead2b3d15984e82ad5753290f6f01b552569
Aadishjain91 <aadish01100@gmail.com>
2024-04-10 12:35:18 -0600

    week-8

--- app/page.js
+++ app/page.js
@@ -1,8 +1,10 @@
 "use client";
 import Link from "next/link";
+// import './globals.css'
 export default function Page() {
     return (
         <main>
+            <link rel="stylesheet" href="./styles.css" />
             <h1>CPRG 306:Assignments</h1>
             <ul>
                 <li>
@@ -12,8 +14,10 @@ export default function Page() {
                     <h2><Link href="week-5">Week 5 Assignment</Link></h2>
                     <h2><Link href="week-6">Week 6 Assignment</Link></h2>
                     <h2><Link href="week-7">Week 7 Assignment</Link></h2>
+                    <h2><Link href="week-8">Week 8 Assignment</Link></h2>
                 </li>
             </ul>
         </main>
     );
 }
+// Path: app/week-5/item-list.js
---
+++ app/week-8/page.js
@@ -0,0 +1,32 @@
+"use client"
+import React from "react";
+import { useUserAuth } from "./_utils/auth-context";
+
+
+const Page = () => {
+    const { user, gitHubSignIn, firebaseSignOut } = useUserAuth();
+
+    const handleLogin = async () => {
+        await gitHubSignIn();
+    };
+
+    const handleLogout = async () => {
+        await firebaseSignOut();
+    };
+
+    return (
+        <div>
+            {user ? (
+                <div>
+                    <p>Welcome, {user.displayName} ({user.email})</p><br></br>
+                    <button onClick={handleLogout}>Logout</button><br></br>
+                    <a href="./week-8/shopping-list">Click here to see shopping list</a> <br></br>
+                </div>
+            ) : (
+                <button onClick={handleLogin}>Login with GitHub</button>
+            )}
+        </div>
+    );
+};
+
+export default Page;
